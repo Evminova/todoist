88f53e3237423ac874dc98ad6e9f1707891fc9d3
i.evminova <evminova.iryna@gmail.com>
2025-07-14 10:56:39 +0300

    add tests to LoginTest API

--- src/test/java/com/todoist/api/LoginTest.java
+++ src/test/java/com/todoist/api/LoginTest.java
@@ -5,13 +5,14 @@ import com.todoist.utils.AuthDataGenerator;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class LoginTest extends BaseAPI {
 
     @Test
-    public void testLoginWithInvalidPassword() {
-        String randomEmail = AuthDataGenerator.generateEmail(5,10);
-        String invalidPassword = AuthDataGenerator.generatePassword(8,12);
+    public void testInvalidPassword() {
+        String randomEmail = AuthDataGenerator.generateEmail(5, 10);
+        String invalidPassword = AuthDataGenerator.generatePassword(8, 15);
 
         LoginPage.AuthRequest authRequest = new LoginPage.AuthRequest(randomEmail, invalidPassword);
 
@@ -21,6 +22,54 @@ public class LoginTest extends BaseAPI {
                 .post("/user/login")
                 .then()
                 .statusCode(401)
+                .log().all();
+    }
+
+    @Test
+    public void testInvalidEmailFormat() {
+        String invalidEmail = AuthDataGenerator.generateInvalidEmail();
+        String password = AuthDataGenerator.generatePassword(8, 15);
+
+        LoginPage.AuthRequest authRequest = new LoginPage.AuthRequest(invalidEmail, password);
+
+        given()
+                .body(authRequest)
+                .when()
+                .post("/user/login")
+                .then()
+                .statusCode(400)
+                .body("error", equalTo("Email is invalid"))
+                .log().all();
+    }
+
+    @Test
+    public void testEmptyEmail() {
+        String emptyEmail = "";
+        String password = AuthDataGenerator.generatePassword(8, 15);
+
+        LoginPage.AuthRequest authRequest = new LoginPage.AuthRequest(emptyEmail, password);
+
+        given()
+                .body(authRequest)
+                .when()
+                .post("/user/login")
+                .then()
+                .statusCode(400)
+                .body("error", equalTo("Email is invalid"))
+                .log().all();
+    }
+
+    @Test
+    public void testNullEmailPassword() {
+        String nullEmail = null;
+        String nullPassword = null;
+
+        given()
+                .when()
+                .post("/user/login")
+                .then()
+                .statusCode(400)
+                .body("error", equalTo("Required argument is missing"))
                 .log().all();
     }
 }
